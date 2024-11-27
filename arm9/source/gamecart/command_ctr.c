dceed6c5fe2d8a73af5feb3a3f081e0896149d71
skawo <43761362+skawo@users.noreply.github.com>
2024-11-27 10:08:11 +0100

    Increase CTRCARD clock from 4.18 to 13.4 MHz on ROM reads

--- arm9/source/gamecart/command_ctr.c
+++ arm9/source/gamecart/command_ctr.c
@@ -13,6 +13,8 @@ static int refresh_count = 0;
 int refresh_call_every = 10000;
 bool force_refresh = false;
 
+#define HUNDRED_REFRESHES
+
 static void CTR_CmdC5()
 {
     static const u32 c5_cmd[4] = { 0xC5000000, 0x00000000, 0x00000000, 0x00000000 };
@@ -23,9 +25,19 @@ void CTR_CmdReadData(u32 sector, u32 length, u32 blocks, void* buffer)
 {
     if(read_count++ >= refresh_call_every || force_refresh)
     {
-        CTR_CmdC5();
+
+#       ifdef HUNDRED_REFRESHES
+        for (int i = 0; i < 100; i++)
+        {
+            refresh_count += 99;
+#       endif
+            refresh_count++;
+            CTR_CmdC5();
+#       ifdef HUNDRED_REFRESHES
+        }
+#       endif
+
         read_count = 0;
-        refresh_count++;
 
         char tempstr[64];
         snprintf(tempstr, 64, "%d", refresh_count);
